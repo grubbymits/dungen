2f1cd180dc8ed1c03c5721cb0c56094b19972245
Sam Parker <sam@codingbear.uk>
2016-02-09 18:23:05 +0000

    added defintions

---
+++ definitions.js
@@ -0,0 +1,75 @@
+"use strict";
+
+const ENEMY_NAMES = [ "Rat",
+                      "Spiders",
+                      "Lizard",
+                      "Spider Champion",
+                      "Toad",
+                      "Scarab",
+                      "Centipede",
+                      "Serpent",    // 7
+                      "Mushroom",
+                      "Rabbit",
+                      "Bat",
+                      "Bat Champion",
+                      "Snake",
+                      "Wolf",
+                      "Wild Boar",
+                      "Bear",        // 15
+                      "Slimes",
+                      "Slime Champion",
+                      "Scorpion",
+                      "Kraken",
+                      "Vampire",
+                      "Mummy",
+                      "Wraith",
+                      "Carabia",      // 23
+                      "Goblin",
+                      "Zombie",
+                      "Undead",
+                      "Orc",
+                      "Cyclop",
+                      "Werewolf",
+                      "Golem",
+                      "Demon" ];
+
+const RAT = 0;
+const SPIDERS = 1;
+const LIZARD = 2;
+const SPIDER_CHAMPION = 3;
+const TOAD = 4;
+const SCARAB = 5;
+const CENTIPEDE = 6;
+const SERPENT = 7;
+const MUSHROOM = 8;
+const RABBIT = 9;
+const BAT = 10;
+const BAT_CHAMPION = 11;
+const SNAKE = 12;
+const WOLF = 13;
+const WILD_BOAR = 14;
+const BEAR = 15;
+const SLIMES = 16;
+const SLIME_CHAMPION = 17;
+const SCORPION = 18;
+const KRAKEN = 19;
+const VAMPIRE = 20;
+const MUMMY = 21;
+const WRAITH = 22;
+const CARABIA = 23;
+const GOBLIN = 24;
+const ZOMBIE = 25;
+const UNDEAD = 26;
+const ORC = 27;
+const CYCLOP = 28;
+const WEREWOLF = 29;
+const GOLEM = 30;
+const DEMON = 31;
+
+const ROGUE = 0;
+const WARLOCK = 1;
+const BERSERKER = 2;
+const ARCHER = 4;
+const KNIGHT = 5;
+const WIZARD = 6;
+const BLACK_MAGE = 7;
